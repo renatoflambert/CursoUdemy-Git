5b6c64d4eb2f4d145d33521585a7884c6a799cbe
Renato Lambert <renatoflambert@gmail.com>
2019-09-18 15:59:40 -0300

    Inclusao do DigiteAlgo

--- IntroGit/Program.cs
+++ IntroGit/Program.cs
@@ -7,6 +7,7 @@ namespace IntroGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello GitHub!");
+            Console.WriteLine("Digite algo: ");
             Console.ReadLine();
         }
     }
